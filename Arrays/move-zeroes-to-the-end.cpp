3dc8fca515a6fa407443d4e833c8344c4cb8caa4
Nitin Srinivas <146994721+NITIN-SRINIVAS-13@users.noreply.github.com>
2025-07-12 00:45:47 +0530

    Added LeetCode 283 C++ solution to Arrays folder

---
+++ Arrays/move-zeroes-to-the-end.cpp
@@ -0,0 +1,16 @@
+// LEETCODE 283 : MOVE ZEROES
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroIndex = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[lastNonZeroIndex++] = nums[i];
+            }
+        }
+        for (int i = lastNonZeroIndex; i < nums.size(); i++) {
+            nums[i] = 0;
+        }
+    }
+};
