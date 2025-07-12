5e876a04200f793168d2819d302a3237c07ba4d1
Nitin Srinivas <146994721+NITIN-SRINIVAS-13@users.noreply.github.com>
2025-07-12 22:33:02 +0530

    Added LeetCode 136 C++ solution to Bit Manipulation folder

---
+++ Bit Manipulation/find-the-one-time-occurring-number.cpp
@@ -0,0 +1,12 @@
+// Leetcode 136 : Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+};
