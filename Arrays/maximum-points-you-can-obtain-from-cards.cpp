a9ab1bd13eb79427c1557cfaf6e29da8ce3012d2
Nitin Srinivas <146994721+nitttinnnnnn@users.noreply.github.com>
2025-07-19 21:59:44 +0530

    Added solution to leetcode 1423 problem

--- Arrays/maximum-points-you-can-obtain-from-cards.cpp
+++ Arrays/maximum-points-you-can-obtain-from-cards.cpp
@@ -1,1 +1,22 @@
-// Leetcode : 
+// Leetcode: 1423 Maximum points you can obtain from cards
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int lsum = 0;
+        int rsum = 0;
+        int maxSum = 0;
+        for(int i=0; i<k; i++){
+            lsum += cardPoints[i];
+        }
+        maxSum = lsum;
+        int rightIndex = cardPoints.size()-1;
+        for(int i=k-1; i>=0; i--){
+            lsum -= cardPoints[i];
+            rsum += cardPoints[rightIndex];
+            rightIndex--;
+            maxSum = max(maxSum , lsum + rsum);
+        }
+        return maxSum;        
+    }
+};
