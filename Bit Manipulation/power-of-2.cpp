86a8137de353a1d4298379af7c437c5f36821b46
Nitin Srinivas <146994721+nitttinnnnnn@users.noreply.github.com>
2025-07-24 18:37:57 +0530

    Added LeetCode 231 C++ solution to Bit Manipulation folder

---
+++ Bit Manipulation/power-of-2.cpp
@@ -0,0 +1,10 @@
+// Leetcode 231: Power of two 
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        if (n <= 0) return false;
+        return (n & (n-1)) == 0;
+    }
+};
