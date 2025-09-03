658a9ae197fc670d5831adc9491e9320642493cf
Nitin Srinivas <146994721+nitttinnnnnn@users.noreply.github.com>
2025-09-03 19:28:05 +0530

    Added LeetCode 144 C++ solution to Trees folder

---
+++ Trees/Traversals/Preorder-Traversal.cpp
@@ -0,0 +1,26 @@
+
+struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+class Solution {
+public:
+
+    void traversal(TreeNode* root, vector<int>& answer){
+        if(root == NULL) return;
+        answer.push_back(root -> val);
+        traversal(root -> left , answer);
+        traversal(root -> right , answer);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        traversal(root , answer);
+        return answer;
+    }
+};
